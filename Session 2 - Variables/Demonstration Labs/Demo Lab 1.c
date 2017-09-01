b464b12ce65a71626df9830207d78320f074c813
JCantu248 <JCantu248@gmail.com>
2017-09-01 13:42:50 -0500

    Add files via upload

---
+++ Session 2 - Variables/Demonstration Labs/Demo Lab 1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int integer = 1;
+	float singlePrecision = 2.2;
+	double doublePrecision = 3.3;
+	char singleChar = '$';
+	char singleChar2 = 33;
+	
+	printf("Your integer is %d \n", integer);
+	printf("Your float is %f \n", singlePrecision);
+	printf("Your double is %lf \n", doublePrecision);
+	printf("Your first char is %c \n", singleChar);
+	printf("Your second char is %c \n", singleChar2);  
+	
+	//I OWE EVERYONE PIZZA AND BEER FOR LEAVING MY COMPUTER UNLOCKED
+
+	return 0;
+}
